5fd240645bc4df085c497557eaf168a92ce3ce7a
Joschka Lingemann <joschka.lingemann@cern.ch>
2015-06-08 19:21:50 +0200

    Internal muon now stores regional cand to avoid copy.

--- DataFormats/L1TMuon/interface/L1TGMTInternalMuon.h
+++ DataFormats/L1TMuon/interface/L1TGMTInternalMuon.h
@@ -2,59 +2,67 @@
 #define __l1t_gmt_internal_muon_h__
 
 #include "DataFormats/L1TMuon/interface/L1TRegionalMuonCandidate.h"
+#include <utility>
 
 namespace l1t {
 class L1TGMTInternalMuon : public L1TRegionalMuonCandidate {
   public:
-    L1TGMTInternalMuon() : 
-      L1TRegionalMuonCandidate(), hwRank_(0), hwCancelBit_(0), hwWins_(0), 
-        hwIsoSum_(0), hwDeltaEta_(0), hwDeltaPhi_(0), hwAbsIso_(0), hwRelIso_(0), hwCaloIndex_(-1,-1)
-      {};
-
-    L1TGMTInternalMuon(int pt, int phi, int eta, int sign, int signvalid, int quality, int processor, tftype trackFinder, int rank, int cancelbit) : 
-      L1TRegionalMuonCandidate(pt, phi, eta, sign, signvalid, quality, processor, trackFinder), hwRank_(rank), hwCancelBit_(cancelbit), hwIsoSum_(0), 
-        hwDeltaEta_(0), hwDeltaPhi_(0), hwAbsIso_(0), hwRelIso_(0), hwCaloIndex_(-1,-1)
-      {};
-
-    L1TGMTInternalMuon(const L1TRegionalMuonCandidate& other) : L1TRegionalMuonCandidate(other), hwRank_(0), hwCancelBit_(0), hwIsoSum_(0), hwDeltaEta_(0), hwDeltaPhi_(0), hwAbsIso_(-1), hwRelIso_(-1)
-    {};
+    explicit L1TGMTInternalMuon(const L1TRegionalMuonCandidate& regional);
 
     virtual ~L1TGMTInternalMuon() {};
 
-    void setHwCancelBit(int bit) { hwCancelBit_ = bit; };
-    void setHwRank(int bits) { hwRank_ = bits; };
-    void setHwWins(int wins) { hwWins_ = wins; };
-    void increaseWins() { hwWins_++; };
-    void setHwIsoSum(int isosum) { hwIsoSum_ = isosum; };
-    void setHwAbsIso(int iso) { hwAbsIso_ = iso; };
-    void setHwRelIso(int iso) { hwRelIso_ = iso; };
-    void setExtrapolation(int deta, int dphi) { hwDeltaEta_ = deta; hwDeltaPhi_ = dphi; };
-    void setHwCaloEta(int idx) { hwCaloIndex_.second = idx; }
-    void setHwCaloPhi(int idx) { hwCaloIndex_.first = idx; }
+    void setHwCancelBit(int bit) { m_hwCancelBit = bit; };
+    void setHwRank(int bits) { m_hwRank = bits; };
+    void setHwWins(int wins) { m_hwWins = wins; };
+    void increaseWins() { m_hwWins++; };
+    void setHwIsoSum(int isosum) { m_hwIsoSum = isosum; };
+    void setHwAbsIso(int iso) { m_hwAbsIso = iso; };
+    void setHwRelIso(int iso) { m_hwRelIso = iso; };
+    void setExtrapolation(int deta, int dphi) { m_hwDeltaEta = deta; m_hwDeltaPhi = dphi; };
+    void setHwCaloEta(int idx) { hwCaloIndex_.second = idx; };
+    void setHwCaloPhi(int idx) { hwCaloIndex_.first = idx; };
 
-    const int hwCancelBit() const { return hwCancelBit_; };
-    const int hwRank() const { return hwRank_; };
-    const int hwWins() const { return hwWins_; };
-    const int hwIsoSum() const { return hwIsoSum_; };
-    const int hwDEta() const { return hwDeltaEta_; };
-    const int hwDPhi() const { return hwDeltaPhi_; };
-    const int hwAbsIso() const { return hwAbsIso_; };
-    const int hwRelIso() const { return hwRelIso_; };
-    const int hwCaloEta() const { return hwCaloIndex_.second; }
-    const int hwCaloPhi() const { return hwCaloIndex_.first; }
+    const int link() const { return m_link; }
+    const int hwCancelBit() const { return m_hwCancelBit; };
+    const int hwRank() const { return m_hwRank; };
+    const int hwWins() const { return m_hwWins; };
+    const int hwIsoSum() const { return m_hwIsoSum; };
+    const int hwDEta() const { return m_hwDeltaEta; };
+    const int hwDPhi() const { return m_hwDeltaPhi; };
+    const int hwAbsIso() const { return m_hwAbsIso; };
+    const int hwRelIso() const { return m_hwRelIso; };
+    const int hwCaloEta() const { return hwCaloIndex_.second; };
+    const int hwCaloPhi() const { return hwCaloIndex_.first; };
+
+    const L1TRegionalMuonCandidate& origin() const { return m_regional; };
     
+    inline const int hwPt() const { return m_regional.hwPt(); };
+    inline const int hwPhi() const { return m_regional.hwPhi(); };
+    inline const int hwEta() const { return m_regional.hwEta(); };
+    inline const int hwSign() const { return m_regional.hwSign(); };
+    inline const int hwSignValid() const { return m_regional.hwSignValid(); };
+    inline const int hwQual() const { return m_regional.hwQual(); };
+    inline const int hwTrackAddress() const { return m_regional.hwTrackAddress(); };
+    inline const int processor() const { return m_regional.processor(); };
+    inline const tftype trackFinderType() const { return m_regional.trackFinderType(); };
+
   private:
-    int hwRank_;
-    int hwCancelBit_;
-    int hwWins_;
-    int hwIsoSum_;
-    int hwDeltaEta_;
-    int hwDeltaPhi_;
-    int hwAbsIso_;
-    int hwRelIso_;
+    L1TGMTInternalMuon();
+    void initLink();
+    
+    const L1TRegionalMuonCandidate& m_regional;
+    int m_hwRank;
+    int m_hwCancelBit;
+    int m_hwWins;
+    int m_hwIsoSum;
+    int m_hwDeltaEta;
+    int m_hwDeltaPhi;
+    int m_hwAbsIso;
+    int m_hwRelIso;
+    int m_link;
     std::pair<int, int> hwCaloIndex_;
 };
 
-}
+} // namespace l1t
 
 #endif /* define __l1t_gmt_internal_muon_h__ */
