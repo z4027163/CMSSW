bd61a3ee8f1d57e16500457ab12eab8c279901f7
matthew <mattcarver44@gmail.com>
2015-07-06 13:34:03 +0200

    Updated RunTrackFinder.py to reflect usage of RelVals and performing TP production in same sequence as TF

--- L1Trigger/L1EndcapMuonTrackFinder/test/RunTrackFinder.py
+++ L1Trigger/L1EndcapMuonTrackFinder/test/RunTrackFinder.py
@@ -7,9 +7,10 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10))
 #process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",ignoreTotal = cms.untracked.int32(1))
 process.load("L1TriggerConfig.L1ScalesProducers.L1MuTriggerScalesConfig_cff")
 process.load("L1TriggerConfig.L1ScalesProducers.L1MuTriggerPtScaleConfig_cff")
-process.load("Configuration.StandardSequences.Geometry_cff")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-
+process.load('L1Trigger.L1EndcapMuonTrackFinder.L1TMuonTriggerPrimitiveProducer_cfi')
+process.load('Configuration.Geometry.GeometryExtendedPostLS1Reco_cff')
+process.load('Configuration.Geometry.GeometryExtendedPostLS1_cff')
 
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
@@ -44,8 +45,13 @@ process.L1TMuonEndcapTrackFinder = cms.EDProducer(
 
 process.content = cms.EDAnalyzer("EventContentAnalyzer")
 
-#infile = 'file:/afs/cern.ch/work/m/mcarver/MuonDevelopment/CMSSW_7_2_0_pre6/src/L1Trigger/L1TMuon/test/Sector1Events/1p2_2p0_Job1.root'
-infile = 'file:MuGun_test.root'
+infile = ['/store/relval/CMSSW_7_5_0_pre1/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RECO/MCRUN2_74_V7_FastSim-v1/00000/04DB6E17-72E2-E411-8311-0025905964BA.root',
+       '/store/relval/CMSSW_7_5_0_pre1/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RECO/MCRUN2_74_V7_FastSim-v1/00000/24978F06-72E2-E411-8346-0025905A6084.root',
+       '/store/relval/CMSSW_7_5_0_pre1/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RECO/MCRUN2_74_V7_FastSim-v1/00000/469C811A-72E2-E411-B1EF-0025905A6118.root',
+       '/store/relval/CMSSW_7_5_0_pre1/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RECO/MCRUN2_74_V7_FastSim-v1/00000/AAD41A17-72E2-E411-A617-0025905A607E.root',
+       '/store/relval/CMSSW_7_5_0_pre1/RelValSingleMuPt100_UP15/GEN-SIM-DIGI-RECO/MCRUN2_74_V7_FastSim-v1/00000/C4F4E747-71E2-E411-8305-0026189438AB.root' ]
+#'file:/afs/cern.ch/work/m/mcarver/MuonDevelopment/CMSSW_7_2_0_pre6/src/L1Trigger/L1TMuon/test/Sector1Events/1p2_2p0_Job1.root'
+#infile = 'file:MuGun_test.root'
 fileOutName = "Histos.root"
 
 process.source = cms.Source(
@@ -74,7 +80,8 @@ process.FEVTDEBUGoutput = cms.OutputModule(
     )
 )
 
-process.L1TMuonSequence = cms.Sequence(process.L1TMuonEndcapTrackFinder)
+process.L1TMuonSequence = cms.Sequence(process.L1TMuonTriggerPrimitives + 
+									process.L1TMuonEndcapTrackFinder)
 
 process.L1TMuonPath = cms.Path(process.L1TMuonSequence)
 
