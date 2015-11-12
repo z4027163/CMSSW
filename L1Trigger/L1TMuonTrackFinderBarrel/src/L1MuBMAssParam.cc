36d629f02e1afbb5d8dcb22efeba4b55344ccc8b
gflouris <flourisj@gmail.com>
2015-11-12 18:23:00 +0100

    clean up

--- L1Trigger/L1TMuonTrackFinderBarrel/src/L1MuBMAssParam.cc
+++ L1Trigger/L1TMuonTrackFinderBarrel/src/L1MuBMAssParam.cc
@@ -35,22 +35,7 @@ ostream& operator<<( ostream& s, PtAssMethod method) {
     case PT24H  : { return s << "PT24H "; break; }
     case PT34L  : { return s << "PT34L "; break; }
     case PT34H  : { return s << "PT34H "; break; }
-    /*case PT12LO : { return s << "PT12LO "; break; }
-    case PT12HO : { return s << "PT12HO "; break; }
-    case PT13LO : { return s << "PT13LO "; break; }
-    case PT13HO : { return s << "PT13HO "; break; }
-    case PT14LO : { return s << "PT14LO "; break; }
-    case PT14HO : { return s << "PT14HO "; break; }
-    case PT23LO : { return s << "PT23LO "; break; }
-    case PT23HO : { return s << "PT23HO "; break; }
-    case PT24LO : { return s << "PT24LO "; break; }
-    case PT24HO : { return s << "PT24HO "; break; }
-    case PT34LO : { return s << "PT34LO "; break; }
-    case PT34HO : { return s << "PT34HO "; break; }
-    case PT15LO : { return s << "PT15LO "; break; }
-    case PT15HO : { return s << "PT15HO "; break; }
-    case PT25LO : { return s << "PT25LO "; break; }
-    case PT25HO : { return s << "PT25HO "; break; }*/
+
     default :
       return s << "unknown pt-assignment method ";
   }
