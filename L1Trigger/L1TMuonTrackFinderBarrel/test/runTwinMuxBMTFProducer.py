a9406e9982d1b74cdd0e796434d76acef74ce2b9
gflouris <flourisj@gmail.com>
2015-11-12 18:11:46 +0100

    es producer

--- L1Trigger/L1TMuonTrackFinderBarrel/test/runTwinMuxBMTFProducer.py
+++ L1Trigger/L1TMuonTrackFinderBarrel/test/runTwinMuxBMTFProducer.py
@@ -10,12 +10,12 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(50)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
-process.source = cms.Source(
-    'PoolSource',
-fileNames = cms.untracked.vstring('file:///afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_50k_eta08.root')
-    )
+process.source = cms.Source('PoolSource',
+#				fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_50k_eta08.root')
+ fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/g/gflouris/public/SingleMuPt6180_noanti_10k_eta1.root')
+	                    )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500))
 
 # PostLS1 geometry used
 process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
@@ -25,25 +25,38 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
+
+####Event Setup Producer
+process.load('L1Trigger.L1TMuonTrackFinderBarrel.l1tmbtfparamsproducer_cfi')
+process.esProd = cms.EDAnalyzer("EventSetupRecordDataGetter",
+   toGet = cms.VPSet(
+      cms.PSet(record = cms.string('L1TBMTFParamsRcd'),
+               data = cms.vstring('L1TBMTFParams'))
+                   ),
+   verbose = cms.untracked.bool(True)
+)
+
+
 ####BMTF Emulator
 process.load('L1Trigger.L1TMuonTrackFinderBarrel.bmtfDigis_cfi')
-process.bmtfEmulator = cms.EDProducer("BMTrackFinder",
-   DTDigi_Source = cms.InputTag("L1TTwinMuxProducer"),
-   DTDigi_Theta_Source = cms.InputTag("simDtTriggerPrimitiveDigis"),
-   Debug = cms.untracked.int32(0)
-)
+process.bmtfDigis.DTDigi_Source = cms.InputTag("L1TTwinMuxProducer")
+process.bmtfDigis.DTDigi_Theta_Source = cms.InputTag("simDtTriggerPrimitiveDigis")
+process.bmtfDigis.Debug = cms.untracked.int32(0)
+
+
 ####TwinMux Emulator
 process.load('L1Trigger.L1TMuonTrackFinderBarrel.L1TTwinMuxProducer_cfi')
 
 
-process.L1TMuonSeq = cms.Sequence( process.L1TTwinMuxProducer +
-                                   process.bmtfEmulator 
+process.L1TMuonSeq = cms.Sequence( process.esProd             +
+				   process.L1TTwinMuxProducer +
+                                   process.bmtfDigis 
 )
 
 process.L1TMuonPath = cms.Path(process.L1TMuonSeq)
 
 process.out = cms.OutputModule("PoolOutputModule", 
-   fileName = cms.untracked.string("l1tbmtf_superprimitives.root"),
+   fileName = cms.untracked.string("l1tbmtf_superprimitives1.root"),
                                )
 
 process.output_step = cms.EndPath(process.out)
