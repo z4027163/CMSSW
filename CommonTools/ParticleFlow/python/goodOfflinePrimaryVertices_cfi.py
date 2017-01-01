5e17e9e157ba96a5448a28632384ddf7e7ec6067
Zhangqier <zqewang@ucdavis.edu>
2017-01-01 23:39:18 +0100

    Ntupler MiniAOD input update for vertex

--- CommonTools/ParticleFlow/python/goodOfflinePrimaryVertices_cfi.py
+++ CommonTools/ParticleFlow/python/goodOfflinePrimaryVertices_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 goodOfflinePrimaryVertices = cms.EDFilter("VertexSelector",
-   src = cms.InputTag("offlinePrimaryVertices"),
+   src = cms.InputTag("offlineSlimmedPrimaryVertices"),
    cut = cms.string("!isFake && ndof >= 4.0 && abs(z) <= 24.0 && abs(position.Rho) <= 2.0"),
    filter = cms.bool(False)
 )
